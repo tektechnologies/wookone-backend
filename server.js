b91895bad88b1fd085d1d06b5980f0b5c732b954
tektechnologies <info@tektechnologies.com>
2023-01-26 12:30:47 -0600

    add env file

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+console.log('server file logging');
+
+// requires
+const express = require('express');
+//app is my server
+const app = express();
+
+
+const PORT = process.env.PORT;
+
+
+
+
+// endpoints
+// error handling
+// server start
