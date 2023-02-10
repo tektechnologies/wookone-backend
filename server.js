5d2ed2430e43f2f5141e360a4dc6c59adf30cb2e
tektechnologies <info@tektechnologies.com>
2023-02-09 22:28:27 -0600

    clean code up

--- server.js
+++ server.js
@@ -10,39 +10,32 @@ const movies = require('./modules/movies');
 const PORT = process.env.PORT || 5005;
 const cors = require('cors');
 
-
 app.use(cors());
-
-
-
-
 
 app.get('/weather', getWeather);
 app.get('/movies', getMovies);
 
-function getWeather(request,response) {
+function getWeather(request, response) {
   // console.log('!!!!!!!!!! ',request.query);
   const { lat, lon } = request.query;
   // console.log(lat,lon);
   weather(lat, lon)
-    .then(summaries => response.status(200).send(summaries))
+    .then((summaries) => response.status(200).send(summaries))
     .catch((error) => {
       console.error(error);
       response.status(500).send('Sorry. Something went wrong!');
     });
 }
 
-
-function getMovies(request, response){
-  console.log('request object',request.query.searchQuery);
-  const location =request.query.searchQuery;
-  movies(location).then(movieList => response.status(200).send(movieList))
-    .catch(error => {
+function getMovies(request, response) {
+  // console.log('request object',request.query.searchQuery);
+  const location = request.query.searchQuery;
+  movies(location)
+    .then((movieList) => response.status(200).send(movieList))
+    .catch((error) => {
       console.error(error);
       response.status(500).send('OPPS! 500' + error.message);
     });
-
 }
-
 
 app.listen(PORT, () => console.log(`PORT RUN: ${PORT}`));
