875fa59141088b896b381cc1c3b973713b6784b9
tektechnologies <info@tektechnologies.com>
2023-02-13 22:22:35 -0600

    add yelp

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 require('dotenv').config();
 const weather = require('./modules/weather.js');
 const movies = require('./modules/movies');
+const yelp = require('./modules/yelp.js');
 const PORT = process.env.PORT || 5005;
 const cors = require('cors');
 
@@ -13,6 +14,9 @@ app.use(cors());
 
 app.get('/weather', getWeather);
 app.get('/movies', getMovies);
+app.get('/yelp', yelpHandler);
+
+
 
 function getWeather(request, response) {
   const { lat, lon } = request.query;
@@ -33,5 +37,18 @@ function getMovies(request, response) {
       response.status(500).send('OPPS! 500' + error.message);
     });
 }
+
+
+function yelpHandler(request, response) {
+  const location = request.query.searchQuery;
+  yelp(location, request.query.page)
+    .then(reviews => response.send(reviews))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!')
+    });
+}
+
+
 
 app.listen(PORT, () => console.log(`PORT RUN: ${PORT}`));
